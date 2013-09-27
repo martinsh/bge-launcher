a8fdc32268c5a93d1bb06ed2c4aaf60197643ef1
andreasesau@gmail.com <andreasesau@gmail.com@d84a0d31-0d20-f993-378d-66d483ceb3e8>
2013-09-27 23:22:18 +0000

    Added the current state of the project

---
+++ trunk/BGE_Launcher.py
@@ -0,0 +1,224 @@
+#!/usr/bin/python2.7
+import wx
+import wx.xrc
+import sys
+import subprocess
+from ui import MainFrame
+import sys, string, os
+import zipfile
+import shutil
+from xml.dom import minidom
+import tempfile
+
+	
+class MainFrame2(MainFrame):
+	def __init__(self,parent):
+		super(MainFrame2,self).__init__(parent)
+		### zip pathes
+		self.resourcePath = os.path.dirname(__file__)
+		self.filename = os.path.basename(self.resourcePath)
+		self.folderpath = os.path.join(os.getcwd(),"data","player")
+		self.filepath = os.path.join(self.folderpath,"game.data")
+		
+		self.tmpFolder = tempfile.mkdtemp()
+
+		### write XML file
+		try:
+			f = open("./data/player/config.xml","r")
+			print("file found")
+		except:
+			print("writing new file")
+			self.initXML()
+		
+		### launcher settings
+		doc = minidom.parse(os.path.join(self.folderpath,"config.xml"))
+		self.window_title=doc.getElementsByTagName('title')[0].attributes['value'].value
+		self.blenderplayer=doc.getElementsByTagName('blenderplayer')[0].attributes['value'].value
+		self.blendfile=doc.getElementsByTagName('blendfile')[0].attributes['value'].value
+		self.show_framerate=int(doc.getElementsByTagName('show_framerate')[0].attributes['value'].value)
+		self.nomipmap=int(doc.getElementsByTagName('nomipmap')[0].attributes['value'].value)
+		self.splash_screen = doc.getElementsByTagName('splash_screen')[0].attributes['value'].value
+		self.aa = int(doc.getElementsByTagName('aa')[0].attributes['value'].value)
+		
+		if doc.getElementsByTagName('fullscreen')[0].attributes['value'].value == 'True':
+			self.fullscreen = True	
+		else:
+			self.fullscreen = False
+		self.res_index = int(doc.getElementsByTagName('res_index')[0].attributes['value'].value)
+		
+		self.resolutions = []
+		for resolution in doc.getElementsByTagName('resolution'):
+			self.resolutions.append(resolution.attributes['value'].value)
+		
+
+		
+		
+		###launcher title
+		self.title = self.window_title
+		
+		### set height and width to splashscreen size
+		self.splash=wx.Image(os.path.join(os.getcwd(),self.splash_screen)).ConvertToBitmap()
+		height = self.splash.GetHeight()
+		width = self.splash.GetWidth()
+		self.SetSizeWH(width, height+150)
+		
+		### Create Dropdownbox with all setable Resolutions
+		for item in self.resolutions:
+			self.m_resolution.Append(item)
+		self.m_resolution.SetSelection(self.res_index)
+		self.m_aa.SetSelection(self.aa)
+		self.m_fullscreen.SetValue(self.fullscreen)
+
+		### pick icon.png that is displayed at the upper left corner
+		try:
+			ico = wx.Icon('./data/images/icon.png', wx.BITMAP_TYPE_PNG)
+			self.SetIcon(ico)
+		except:
+			pass
+		
+
+	### write config file		
+	def initXML(self):
+		doc = minidom.Document()
+		url = ""
+		settings = doc.createElementNS(url,"launcher")
+		doc.appendChild(settings)
+		
+		def createElement(elementName,attributeName,value):
+			item = doc.createElementNS(url,elementName)
+			item.setAttributeNS(url,attributeName,value)
+			settings.appendChild(item)
+		
+		createElement("title","value","BGE Launcher")
+		
+		createElement("blenderplayer","value",os.path.join("data","player","blenderplayer.exe"))
+		createElement("blendfile","value",os.path.join("data","player","data","zipfile.blend"))
+		createElement("nomipmap","value","0")
+		createElement("show_framerate","value","0")
+		createElement("res_index","value","0")
+		createElement("fullscreen","value","False")
+		createElement("aa","value","0")
+		createElement("splash_screen","value",os.path.join(".","data","images","splash.png"))
+		
+		createElement("resolution","value","640x480")
+		createElement("resolution","value","800x600")
+		createElement("resolution","value","1024x768")
+		createElement("resolution","value","1152x864")
+		createElement("resolution","value","1280x720")
+		createElement("resolution","value","1280x768")
+		createElement("resolution","value","1280x800")
+		createElement("resolution","value","1280x960")
+		createElement("resolution","value","1280x1024")
+		createElement("resolution","value","1440x900")
+		createElement("resolution","value","1600x1200")
+		createElement("resolution","value","1680x1050")
+		createElement("resolution","value","1920x1080")
+		createElement("resolution","value","1920x1200")
+		
+		file_object = open(os.path.join(self.folderpath,"config.xml"), "w")
+		file_object.write(doc.toprettyxml());
+		file_object.close()	
+	
+	def saveXML(self):
+		doc = minidom.Document()
+		url = ""
+		settings = doc.createElementNS(url,"launcher")
+		doc.appendChild(settings)
+		
+		def createElement(elementName,attributeName,value):
+			item = doc.createElementNS(url,elementName)
+			item.setAttributeNS(url,attributeName,value)
+			settings.appendChild(item)
+		
+		createElement("title","value",self.window_title)
+		
+		createElement("blenderplayer","value",self.blenderplayer)
+		createElement("blendfile","value",self.blendfile)
+		createElement("nomipmap","value",str(self.nomipmap))
+		createElement("show_framerate","value",str(self.show_framerate))
+		createElement("res_index","value",str(self.m_resolution.GetCurrentSelection()))
+		createElement("aa","value",str(self.m_aa.GetCurrentSelection()))
+		createElement("fullscreen","value",str(self.m_fullscreen.GetValue()))
+		createElement("splash_screen","value",self.splash_screen)
+		
+		for resolution in self.resolutions:
+			createElement("resolution","value",resolution)
+		
+		file_object = open(os.path.join(self.folderpath,"config.xml"), "w")
+		file_object.write(doc.toprettyxml());
+		file_object.close()	
+
+	
+	def deleteTmpFolder(self):
+		if os.path.exists(self.tmpFolder):
+			shutil.rmtree(self.tmpFolder)
+		#if os.path.exists(os.path.join(self.folderpath,"data")):
+		#	shutil.rmtree(os.path.join(self.folderpath,"data"))
+
+	def unzipData(self,folderpath,filename,filepath):
+		zfile = zipfile.ZipFile(filepath)
+		for name in zfile.namelist():
+			(dirname, filename) = os.path.split(name)
+			if filename == "":
+				if not os.path.exists(os.path.join(folderpath,dirname)):
+					os.mkdir(os.path.join(folderpath,dirname))
+			else:
+				fd = open(os.path.join(folderpath,name),'wb')
+				fd.write(zfile.read(name))
+				fd.close()
+		zfile.close()
+	
+			
+	def fullScreenMode(self):
+		self.fullscreen = self.m_fullscreen.GetValue()
+		if self.fullscreen == True:
+			return '-f '
+		else:
+			return '-w '
+	
+	def antiAliasing(self):
+		self.aa = self.m_aa.GetStringSelection()
+		if self.aa == 'off':
+			return '0'
+		else:
+			return self.aa
+	
+	def runBlender(self):
+		self.resolution = self.m_resolution.GetStringSelection()
+		active_res = self.resolution.partition('x')
+		### change to folder to blenderplayer folder
+		os.chdir(os.path.join(os.getcwd(),"data","player"))
+		
+		### blenderplayer command
+		command = ("blenderplayer" + ### Blenderplayer path
+				   ' ' + '-m ' + self.antiAliasing() + ### Antialiasing settings
+				   ' -g show_framerate = ' + str(self.show_framerate) + ### Framerate settings
+				   ' -g nomipmap = '+ str(self.nomipmap) + ### Mipmap settings
+				   ' ' + self.fullScreenMode()  + active_res[0] + ' ' + active_res[2] + ' ' + ### Resolution settings 
+				   os.path.join(self.tmpFolder,self.blendfile)) ### Blendfile path		
+		
+		#print(command)
+		proc = subprocess.Popen(command,shell=True)
+		print(command)
+		stdout,stderr = proc.communicate()
+		if stderr == None:
+			self.deleteTmpFolder()
+			sys.exit(0)
+		
+
+	def OnStartGameClick( self, event ):
+		self.saveXML()
+		#self.unzipData(self.folderpath,self.filename,self.filepath)	
+		self.unzipData(self.tmpFolder,self.filename,self.filepath)	
+		self.Hide()
+		self.runBlender()
+		
+		
+	def OnExitClick( self, event ):
+		self.deleteTmpFolder()
+		sys.exit(0)
+
+app = wx.PySimpleApp()
+frame = MainFrame2(None)
+frame.Show(1)
+app.MainLoop()
