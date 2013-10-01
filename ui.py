4ef773566942a2aecc27b9e8b38e6b67727ee550
andreasesau@gmail.com <andreasesau@gmail.com@d84a0d31-0d20-f993-378d-66d483ceb3e8>
2013-10-01 11:11:10 +0000

    Previously wxPython was used for the ui. I have ported everything to pyqt now.

--- ui.py
+++ ui.py
@@ -1,137 +1,146 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
-###########################################################################
-## Python code generated with wxFormBuilder (version Jun 30 2011)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO "NOT" EDIT THIS FILE!
-###########################################################################
+# Form implementation generated from reading ui file 'Launcher.ui'
+#
+# Created: Tue Oct 01 12:43:57 2013
+#      by: PyQt4 UI code generator 4.9.4
+#
+# WARNING! All changes made in this file will be lost!
 
-import wx
-import wx.xrc
+from PyQt4 import QtCore, QtGui
 
-###########################################################################
-## Class MainFrame
-###########################################################################
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
 
-class MainFrame ( wx.Frame ):
-	
-	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"BGE Launcher", pos = wx.DefaultPosition, size = wx.Size( 500,436 ), style = wx.CAPTION|wx.CLOSE_BOX|wx.MINIMIZE|wx.MINIMIZE_BOX|wx.SYSTEM_MENU|wx.TAB_TRAVERSAL )
-		
-		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
-		
-		bSizer2 = wx.BoxSizer( wx.VERTICAL )
-		
-		self.m_panel2 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		self.m_panel2.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_APPWORKSPACE ) )
-		
-		bSizer4 = wx.BoxSizer( wx.HORIZONTAL )
-		
-		bSizer28 = wx.BoxSizer( wx.VERTICAL )
-		
-		self.m_splash = wx.StaticBitmap( self.m_panel2, wx.ID_ANY, wx.Bitmap( u"data/images/splash.png", wx.BITMAP_TYPE_ANY ), wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer28.Add( self.m_splash, 0, wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
-		
-		self.m_staticline1 = wx.StaticLine( self.m_panel2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
-		bSizer28.Add( self.m_staticline1, 0, wx.ALIGN_CENTER|wx.ALL|wx.EXPAND, 5 )
-		
-		sizer_resolution = wx.BoxSizer( wx.HORIZONTAL )
-		
-		bSizer8 = wx.BoxSizer( wx.VERTICAL )
-		
-		self.m_label_resolution = wx.StaticText( self.m_panel2, wx.ID_ANY, u"Resolution", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_label_resolution.Wrap( -1 )
-		self.m_label_resolution.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BTNTEXT ) )
-		
-		bSizer8.Add( self.m_label_resolution, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 5 )
-		
-		sizer_resolution.Add( bSizer8, 1, wx.EXPAND, 5 )
-		
-		bSizer7 = wx.BoxSizer( wx.HORIZONTAL )
-		
-		m_resolutionChoices = []
-		self.m_resolution = wx.Choice( self.m_panel2, wx.ID_ANY, wx.Point( -1,-1 ), wx.Size( 160,-1 ), m_resolutionChoices, 0 )
-		self.m_resolution.SetSelection( -1 )
-		bSizer7.Add( self.m_resolution, 0, 0, 5 )
-		
-		sizer_resolution.Add( bSizer7, 0, wx.EXPAND, 5 )
-		
-		bSizer12 = wx.BoxSizer( wx.VERTICAL )
-		
-		self.m_fullscreen = wx.CheckBox( self.m_panel2, wx.ID_ANY, u"Fullscreen", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_fullscreen.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BTNTEXT ) )
-		
-		bSizer12.Add( self.m_fullscreen, 0, wx.ALL, 5 )
-		
-		sizer_resolution.Add( bSizer12, 1, wx.EXPAND, 5 )
-		
-		bSizer28.Add( sizer_resolution, 1, wx.ALIGN_CENTER|wx.ALIGN_CENTER_HORIZONTAL|wx.EXPAND|wx.TOP|wx.BOTTOM, 5 )
-		
-		sizer_aa = wx.BoxSizer( wx.HORIZONTAL )
-		
-		bSizer81 = wx.BoxSizer( wx.VERTICAL )
-		
-		self.m_label_aa = wx.StaticText( self.m_panel2, wx.ID_ANY, u"Anti-Aliasing", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_label_aa.Wrap( -1 )
-		self.m_label_aa.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_BTNTEXT ) )
-		
-		bSizer81.Add( self.m_label_aa, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT, 5 )
-		
-		sizer_aa.Add( bSizer81, 1, wx.ALIGN_CENTER_VERTICAL, 5 )
-		
-		bSizer9 = wx.BoxSizer( wx.VERTICAL )
-		
-		m_aaChoices = [ u"off", u"2", u"4", u"8", u"16" ]
-		self.m_aa = wx.Choice( self.m_panel2, wx.ID_ANY, wx.Point( -1,-1 ), wx.Size( 160,-1 ), m_aaChoices, 0 )
-		self.m_aa.SetSelection( 0 )
-		bSizer9.Add( self.m_aa, 0, 0, 5 )
-		
-		sizer_aa.Add( bSizer9, 0, wx.EXPAND, 5 )
-		
-		bSizer11 = wx.BoxSizer( wx.VERTICAL )
-		
-		sizer_aa.Add( bSizer11, 1, wx.EXPAND, 5 )
-		
-		bSizer28.Add( sizer_aa, 1, wx.ALIGN_CENTER|wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.TOP|wx.BOTTOM, 5 )
-		
-		bSizer41 = wx.BoxSizer( wx.HORIZONTAL )
-		
-		self.m_play = wx.Button( self.m_panel2, wx.ID_ANY, u"Play", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer41.Add( self.m_play, 0, wx.ALL, 5 )
-		
-		self.m_exit = wx.Button( self.m_panel2, wx.ID_ANY, u"Quit", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer41.Add( self.m_exit, 0, wx.ALL, 5 )
-		
-		bSizer28.Add( bSizer41, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.TOP|wx.BOTTOM, 5 )
-		
-		bSizer4.Add( bSizer28, 1, wx.EXPAND, 5 )
-		
-		self.m_panel2.SetSizer( bSizer4 )
-		self.m_panel2.Layout()
-		bSizer4.Fit( self.m_panel2 )
-		bSizer2.Add( self.m_panel2, 1, wx.EXPAND, 5 )
-		
-		self.SetSizer( bSizer2 )
-		self.Layout()
-		
-		self.Centre( wx.BOTH )
-		
-		# Connect Events
-		self.Bind( wx.EVT_CLOSE, self.OnExitClick )
-		self.m_play.Bind( wx.EVT_BUTTON, self.OnStartGameClick )
-		self.m_exit.Bind( wx.EVT_BUTTON, self.OnExitClick )
-	
-	def __del__( self ):
-		pass
-	
-	
-	# Virtual event handlers, overide them in your derived class
-	def OnExitClick( self, event ):
-		event.Skip()
-	
-	def OnStartGameClick( self, event ):
-		event.Skip()
-	
-	
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.setWindowModality(QtCore.Qt.NonModal)
+        Form.resize(500, 400)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
+        Form.setMinimumSize(QtCore.QSize(500, 400))
+        Form.setMaximumSize(QtCore.QSize(503, 400))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8("../data/images/icon.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Form.setWindowIcon(icon)
+        self.verticalLayout_3 = QtGui.QVBoxLayout(Form)
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, -1)
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.splash = QtGui.QLabel(Form)
+        self.splash.setMinimumSize(QtCore.QSize(5, 0))
+        self.splash.setMaximumSize(QtCore.QSize(500, 16777215))
+        self.splash.setText(_fromUtf8(""))
+        self.splash.setPixmap(QtGui.QPixmap(_fromUtf8("../data/images/splash.png")))
+        self.splash.setObjectName(_fromUtf8("splash"))
+        self.verticalLayout.addWidget(self.splash)
+        self.line = QtGui.QFrame(Form)
+        self.line.setFrameShape(QtGui.QFrame.HLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName(_fromUtf8("line"))
+        self.verticalLayout.addWidget(self.line)
+        self.verticalLayout_3.addLayout(self.verticalLayout)
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout_3.addItem(spacerItem)
+        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setSpacing(6)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.labelResolution = QtGui.QLabel(Form)
+        self.labelResolution.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.labelResolution.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.labelResolution.setObjectName(_fromUtf8("labelResolution"))
+        self.verticalLayout_2.addWidget(self.labelResolution)
+        self.labelAntiAliasing = QtGui.QLabel(Form)
+        self.labelAntiAliasing.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.labelAntiAliasing.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.labelAntiAliasing.setObjectName(_fromUtf8("labelAntiAliasing"))
+        self.verticalLayout_2.addWidget(self.labelAntiAliasing)
+        self.labelHidden_1 = QtGui.QLabel(Form)
+        self.labelHidden_1.setMaximumSize(QtCore.QSize(16777215, 23))
+        self.labelHidden_1.setText(_fromUtf8(""))
+        self.labelHidden_1.setObjectName(_fromUtf8("labelHidden_1"))
+        self.verticalLayout_2.addWidget(self.labelHidden_1)
+        self.horizontalLayout_3.addLayout(self.verticalLayout_2)
+        self.verticalLayout_5 = QtGui.QVBoxLayout()
+        self.verticalLayout_5.setObjectName(_fromUtf8("verticalLayout_5"))
+        self.resolution = QtGui.QComboBox(Form)
+        self.resolution.setMinimumSize(QtCore.QSize(0, 20))
+        self.resolution.setObjectName(_fromUtf8("resolution"))
+        self.verticalLayout_5.addWidget(self.resolution)
+        self.antiAliasing = QtGui.QComboBox(Form)
+        self.antiAliasing.setMinimumSize(QtCore.QSize(0, 20))
+        self.antiAliasing.setObjectName(_fromUtf8("antiAliasing"))
+        self.antiAliasing.addItem(_fromUtf8(""))
+        self.antiAliasing.addItem(_fromUtf8(""))
+        self.antiAliasing.addItem(_fromUtf8(""))
+        self.antiAliasing.addItem(_fromUtf8(""))
+        self.antiAliasing.addItem(_fromUtf8(""))
+        self.verticalLayout_5.addWidget(self.antiAliasing)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.playButton = QtGui.QPushButton(Form)
+        self.playButton.setMinimumSize(QtCore.QSize(0, 0))
+        self.playButton.setMaximumSize(QtCore.QSize(16777215, 23))
+        self.playButton.setObjectName(_fromUtf8("playButton"))
+        self.horizontalLayout.addWidget(self.playButton)
+        self.quitButton = QtGui.QPushButton(Form)
+        self.quitButton.setObjectName(_fromUtf8("quitButton"))
+        self.horizontalLayout.addWidget(self.quitButton)
+        self.verticalLayout_5.addLayout(self.horizontalLayout)
+        self.horizontalLayout_3.addLayout(self.verticalLayout_5)
+        self.verticalLayout_4 = QtGui.QVBoxLayout()
+        self.verticalLayout_4.setSpacing(6)
+        self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
+        self.fullscreen = QtGui.QCheckBox(Form)
+        self.fullscreen.setMinimumSize(QtCore.QSize(0, 20))
+        self.fullscreen.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.fullscreen.setObjectName(_fromUtf8("fullscreen"))
+        self.verticalLayout_4.addWidget(self.fullscreen)
+        self.labelHidden_2 = QtGui.QLabel(Form)
+        self.labelHidden_2.setMaximumSize(QtCore.QSize(16777215, 23))
+        self.labelHidden_2.setText(_fromUtf8(""))
+        self.labelHidden_2.setObjectName(_fromUtf8("labelHidden_2"))
+        self.verticalLayout_4.addWidget(self.labelHidden_2)
+        self.labelHidden_3 = QtGui.QLabel(Form)
+        self.labelHidden_3.setMaximumSize(QtCore.QSize(16777215, 23))
+        self.labelHidden_3.setText(_fromUtf8(""))
+        self.labelHidden_3.setObjectName(_fromUtf8("labelHidden_3"))
+        self.verticalLayout_4.addWidget(self.labelHidden_3)
+        self.horizontalLayout_3.addLayout(self.verticalLayout_4)
+        self.verticalLayout_3.addLayout(self.horizontalLayout_3)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "BGE Launcher", None, QtGui.QApplication.UnicodeUTF8))
+        self.labelResolution.setText(QtGui.QApplication.translate("Form", "Resolution", None, QtGui.QApplication.UnicodeUTF8))
+        self.labelAntiAliasing.setText(QtGui.QApplication.translate("Form", "Anti-Aliasing", None, QtGui.QApplication.UnicodeUTF8))
+        self.antiAliasing.setItemText(0, QtGui.QApplication.translate("Form", "off", None, QtGui.QApplication.UnicodeUTF8))
+        self.antiAliasing.setItemText(1, QtGui.QApplication.translate("Form", "2", None, QtGui.QApplication.UnicodeUTF8))
+        self.antiAliasing.setItemText(2, QtGui.QApplication.translate("Form", "4", None, QtGui.QApplication.UnicodeUTF8))
+        self.antiAliasing.setItemText(3, QtGui.QApplication.translate("Form", "8", None, QtGui.QApplication.UnicodeUTF8))
+        self.antiAliasing.setItemText(4, QtGui.QApplication.translate("Form", "16", None, QtGui.QApplication.UnicodeUTF8))
+        self.playButton.setText(QtGui.QApplication.translate("Form", "Play", None, QtGui.QApplication.UnicodeUTF8))
+        self.quitButton.setText(QtGui.QApplication.translate("Form", "Quit", None, QtGui.QApplication.UnicodeUTF8))
+        self.fullscreen.setText(QtGui.QApplication.translate("Form", "Fullscreen", None, QtGui.QApplication.UnicodeUTF8))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
 
